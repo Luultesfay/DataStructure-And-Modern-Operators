5cff0f16e65a8d962093771a6a9c90309114644b
luul negasi <luultesfay24@gmail.com>
2021-03-10 22:01:06 -0800

    orderPizza function added

--- starter/spread.js
+++ starter/spread.js
@@ -37,6 +37,11 @@
       console.log(`here is your delicious pasta making using ${ing1}, ${ing2}, ${ing3},`)
 
   }
+  orderPizza:(mainIngradient,...otherIngradent){
+    console.log(mainIngradient);
+    console.log(otherIngradent);
+
+  },
 };
 */
 /// spread operator
@@ -46,6 +51,7 @@ The spread operator allows you to spread out elements of an iterable object such
 //we only uses spread operator in values that seperated by commas
 //eg of spread operator  but will see first the old way
 
+/*
 const arr = [7, 8, 9];
 const badArr = [1, 2, arr[0], arr[1], arr[2]]; //we added the arr array to the badArr using old way
 console.log(badArr); // output  [1 2 7 8 9]
@@ -55,7 +61,7 @@ console.log(badArr); // output  [1 2 7 8 9]
 const newArr = [1, 2, ...arr]; //we added the arr  array to newArray using spread syntax
 console.log(newArr); // outputs  [1 2 7 8 9]
 console.log(...newArr); //1 2 7 8 9   outputs indivdual arrays
-
+*/
 //lets update the mainMenu  and make a newNenu  array and add  food to the newMenu
 const newMenu = [...restaurant.mainMenu, 'Gnocci']; //we use spread operator to add all the mainMenu to the newMenu then added "gnocci"
 console.log(newMenu); //[ "Pizza", "Pasta", "Risotto", "Gnocci" ]
@@ -113,3 +119,66 @@ const restaurantCopy = { ...restaurant }; // shallow copies old restaurant  // t
 restaurantCopy.name = 'restorante Roma'; //we change the name in the copied object
 console.log(restaurantCopy.name); //restorante Roma
 console.log(restaurant.name); //Classico Italiano
+
+///////////  Rest Pattern and parameters
+/*
+ Note that ES6 also has the three dots ( ...) which is a rest parameter that collects all remaining arguments of a function into an array. 
+ 
+ -So the three dots ( ...) represent both the spread operator and the rest parameter.
+
+ -Here are the main differences:
+
+-The spread operator unpacks elements.
+-The rest parametr packs elements into an array.
+-The rest parameters must be the last arguments of a function. However, the spread operator can be anywhere:
+// reference from the web https://www.javascripttutorial.net/es6/javascript-spread/
+*/
+
+//1.destructuring
+
+//spread becouse on right side of the  equal operator =
+//const arryyyss = [1, 2, ...[3, 4]];
+
+//REST , becouse on LEFT side of = equal operator
+//const [a, b, ...others] = [1, 2, 3, 4, 5]; //...others is rest operator
+//console.log(a, b, others);// outpts 1 2 [3,4,5]
+
+//eg for rest operator
+
+//lets  destructure the first and third from mainManu and use rest   ...otherFood  to the remaining food
+//we use here destructuring and rest  and inthe right side spread
+const [pizza, , risotto, ...otherFood] = [
+  // here pasta is skipped
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood); //Pizza Risotto [ "Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad" ]
+
+// rest in objects
+/*
+and now let's do the same in objects because it also works indeed in objects.
+So the difference then of course, is that the remaining elements will be collected
+into a new object and not into a new array.So let's not work here with our opening hours
+and let's say that we want to select only Saturday here and then the rest should go into a new object
+which are the weekdays.So Saturday is weekend and Friday and Thursday are the weekdays
+*/
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays); // { thu: {…}, fri: {…} }  so we get the rest weekdays  and  sat holds saturday which is weekend
+
+// 2. for  functions
+
+const add = function (...numbers) {
+  //we use rest parameter which is ...numbers
+  console.log(numbers);
+};
+add(2, 3); //[ 2, 3 ]
+add(5, 3, 6, 7); //[ 5, 3, 6, 7 ]
+add(3, 5, 9, 8); //[ 3, 5, 9, 8 ]
+
+// lets do practice  with rest again with our restaurant object
+//this time  we order pizza  which must have one engradent . becouse we want to show the rest operator how powerful it is
+// we adding orderPizza at the restaurant object and we will pass argument from here
+
+//so when we pass this argument to the orderPizza function   the  first parameter  becomes mashroom and the remaining is take care  by the rest operator ...otheringradent  [ "oliveoil", "meat", "checken" ]
+restaurant.orderPizza('mashroom', 'oliveoil', 'meat', 'checken'); //mashroom  [ "oliveoil", "meat", "checken" ]
+restaurant.orderPizza('mashroom'); //mashrom []
